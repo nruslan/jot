e16ac211a3ed0b15c45e7af86c4289c8720fe9e3
Ruslan N <6238973+nruslan@users.noreply.github.com>
2019-08-13 13:40:11 -0700

    Lesson 35

---
+++ resources/js/components/InputField.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="relative pb-4">
+        <label :for="name" class="text-blue-500 pt-2 uppercase text-xs font-bold absolute">{{ label }}</label>
+        <input :id="name" type="text" :placeholder="placeholder" v-model="value" @input="updateField()" class="pt-8 w-full border-b text-gray-900 focus:outline-none focus:border-blue-400" :class="errorClassObject()">
+        <p class="text-red-600 text-sm" v-text="errorMessage()">errors</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "InputField",
+
+        props: [
+            'name', 'label', 'placeholder', 'errors'
+        ],
+
+        data: function() {
+            return {
+                value: ''
+            }
+        },
+
+        computed: {
+            hasError: function() {
+                return this.errors && this.errors[this.name] && this.errors[this.name].length > 0;
+            }
+        },
+
+        methods: {
+            updateField: function() {
+                this.clearErrors(this.name);
+                this.$emit('update:field', this.value)
+            },
+
+            errorMessage: function() {
+                if(this.hasError) {
+                    return this.errors[this.name][0];
+                }
+            },
+
+            clearErrors: function() {
+                if(this.hasError) {
+                    this.errors[this.name] = null;
+                }
+            },
+
+            errorClassObject: function() {
+                return {
+                    'error-field': this.hasError
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .error-field {
+        @apply .border-red-500 .border-b-2
+    }
+</style>
---
+++ resources/js/views/ContactsCreate.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <form @submit.prevent="submitForm">
+            <InputField name="name" label="Contact Name" placeholder="Contact Name" :errors="errors" @update:field="form.name = $event" />
+            <InputField name="email" label="Contact Email" placeholder="Contact Email" :errors="errors" @update:field="form.email = $event" />
+            <InputField name="company" label="Company" placeholder="Company" :errors="errors" @update:field="form.company = $event" />
+            <InputField name="birthday" label="Birthday" placeholder="MM/DD/YYYY" :errors="errors" @update:field="form.birthday = $event" />
+            <div class="flex justify-end">
+                <button class="py-2 px-4 rounded text-red-700 border mr-5 hover:border-red-700">Cancel</button>
+                <button class="bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-400">Add New Contact</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    import InputField from "../components/InputField";
+
+    export default {
+        name: "ContactsCreate",
+
+        components: {
+            InputField
+        },
+
+        data: function() {
+            return {
+                form: {
+                    'name': '',
+                    'email': '',
+                    'company': '',
+                    'birthday': ''
+                },
+
+                errors: null
+            }
+        },
+
+        methods: {
+            submitForm: function() {
+                axios.post('/api/contacts', this.form)
+                    .then(response => {
+                        console.log(response);
+                    })
+                    .catch(errors => {
+                        this.errors = errors.response.data.errors;
+                        console.log(errors);
+                    });
+            }
+        }
+    }
+</script>
