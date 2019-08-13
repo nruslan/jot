700f8ff55f771958f39be778ecebb559f2a70dfd
Ruslan N <6238973+nruslan@users.noreply.github.com>
2019-08-13 16:06:58 -0700

    Lesson 36 middle

---
+++ resources/js/views/ContactsShow.vue
@@ -0,0 +1,26 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "ContactsShow",
+
+        mounted() {
+            axios.get('/api/contacts/' + this.$route.params.id)
+                .then(response => {
+                    this.contact = response.data.data;
+                })
+                .catch(errors => {
+                    this.errors = errors.response.data.errors;
+                    console.log(errors);
+                });
+        },
+
+        data: function() {
+            return {
+                contact: null,
+            }
+        }
+    }
+</script>
